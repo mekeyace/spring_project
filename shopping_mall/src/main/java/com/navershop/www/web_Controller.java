677ece4fe3feffb7f75cb24282db3fef98d9b0bf
mekeyace <mekeyace@nate.com>
2024-07-26 11:39:39 +0900

    server setting success

---
+++ shopping_mall/src/main/java/com/navershop/www/web_Controller.java
@@ -0,0 +1,11 @@
+package com.navershop.www;
+import java.io.PrintWriter;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class web_Controller {
+	
+	PrintWriter pw = null;
+	
+	
+}
