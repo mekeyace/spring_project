f43e82ca7dafec627edd2a93cf9bd89ab4f1d479
mekeyace <mekeyace@nate.com>
2024-07-26 11:47:24 +0900

    web_Controller

--- shopping_mall/src/main/java/com/navershop/www/web_Controller.java
+++ shopping_mall/src/main/java/com/navershop/www/web_Controller.java
@@ -1,11 +1,15 @@
 package com.navershop.www;
 import java.io.PrintWriter;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class web_Controller {
 	
 	PrintWriter pw = null;
-	
+	@GetMapping("/ajaxok.do")
+	public String ajaxok() {
+		return null;
+	}
 	
 }
